49a562593755a66492d0758b8dd84eed17ded0b5
Anton <158140747+gott777@users.noreply.github.com>
2024-06-16 22:27:37 +0300

    Update index22.py

--- index22.py
+++ index22.py
@@ -1,15 +1,10 @@
-def process_numbers(numbers):
-    reversed_numbers = []
-    for num in numbers:
-        num_str = str(num)
-        reversed_num_str = num_str[::-1]
-        reversed_num = int(reversed_num_str)
-        if num == reversed_num:
-            print(f"{num} является палиндромом")
-        reversed_numbers.append(reversed_num)
-    return reversed_numbers
 
-# Пример использования
-numbers = [123, 456, 121, 1331,11]
-reversed_numbers = process_numbers(numbers)
-print("Перевернутые числа:", reversed_numbers)
+def itog(summa, procent, date):
+    resalt = summa * (1 + (procent * 100) / 100) ** date
+    print("Итоговая сумма: ", resalt)
+
+summa = int(input("Введите начальную сумму: "))
+procent = float(input("Введите процентную ставку(в виде десятичной дроби): "))
+date = int(input("Введите количество лет: "))
+
+itog(summa, procent, date)
